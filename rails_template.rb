a7b796a69165c4ef68215aa91630ee2897da0c8a
javier <javier.ramirez.gomara@gmail.com>
2009-06-12 14:58:41 +0200

    removing unused code

--- rails_template.rb
+++ rails_template.rb
@@ -3,8 +3,6 @@ gem "twitter"
 plugin 'mailer_twitterable', :git => 'git://github.com/javier/mailer_twitterable.git'
 generate(:mailer_twitterable_yml)
 
-inside("vendor") do
-  puts "\n\nread README under vendor/plugins/mailer_twitterable for instructions and configuration options\n\n"  
-end  
+puts "\n\nread README under vendor/plugins/mailer_twitterable for instructions and configuration options\n\n"  
 
 
